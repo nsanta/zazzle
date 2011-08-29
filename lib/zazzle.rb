46d8ca9640e4be428479cc02ec78675ad1f3853a
Nicolas Santa <nicolas55ar@gmail.com>
2011-08-29 20:59:04 -0300

    first version

--- lib/zazzle.rb
+++ lib/zazzle.rb
@@ -1,5 +1,37 @@
 require "zazzle/version"
 
-module Zazzle
-  # Your code goes here...
+class Zazzle
+
+  class << self
+
+    attr_accessor :associate_id
+
+    def product_link(product_id, page = 'productpage', options = {})
+      params = {:rf => associate_id ,
+                :ax => 'linkover' ,
+                :pd => product_id ,
+                :fwd => page,
+                :ed => true}.merge(options)
+      build_request(params)
+    end
+
+    def buffet_link(category_id, options = {})
+      params = {:rf => associate_id ,
+                :ax => 'designblast' ,
+                :cg => category_id ,
+                :fwd => page,
+                :ed => true}.merge(options)
+      build_request(params)
+    end
+
+
+    private
+
+    def build_request(params = {})
+      ["http://www.zazzle.com/api/create/at-#{associate_id}" , "?"+params.to_a.map{|p| p.join('=')}.join('&')].join()
+    end
+
+  end
+
 end
+
--- lib/zazzle/version.rb
+++ lib/zazzle/version.rb
@@ -1,3 +1,4 @@
-module Zazzle
+class Zazzle
   VERSION = "0.0.1"
 end
+
